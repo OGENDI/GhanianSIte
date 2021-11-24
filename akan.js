5425fb0cd0d35931c718b5aa598fecf8a806b88e
OGENDI <demokonge17@gmail.com>
2021-11-24 11:13:21 +0300

    It will add the required files to the project

--- akan.js
+++ akan.js
@@ -1,21 +1,34 @@
-document.getElementById('user').addEventListener('submit',function(event){
-    event.preventDefault();
-    var dayOfBirth = document.getElementById('DD').value;
-    var monthOfBirth = document.getElementById('MM').value;
-    var yearOfBirth = document.getElementById('YYYY').value;
-    var male = document.getElementById('sex0').value;
-    var female = document.getElementById('sex1').value;
-    var maleNames = maleNames[Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
-    // var gender = document.querySelectorAll('input[name="yourSex"]').value;
-    // alert(gender);
 
-    if(dayOfBirth === '' || monthOfBirth === '' || yearOfBirth === '') {
-        alert('One of the fields is empty!');
-    } else if(dayOfBirth <= 0 || dayOfBirth > 31) {
-        alert('Days cannot be 0 or negative number or cannot be greater than 31!');
-    } else if(monthOfBirth <= 0 || monthOfBirth > 12) {
-        alert('Months cannot be 0 or negative number or cannot be greater than 12');
-    } else if(yearOfBirth.trim().length !== 4) {
-        alert('Year of birth has 4 digits e.g 1980');
-    } else 
-});
+var user = document.getElementById('user');
+user.addEventListener('submit',akanName);
+
+const femalNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const maleName=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Satyrday'];
+
+function akanName(event){
+    event.preventDefault();
+
+    var username = document.getElementById('yourName').value.trim();
+    var doB = document.getElementById('date').value.trim();
+    var gender = document.getElementsByClassName('yourSex')
+
+    // find index of thhe day from the entered dates
+    var my_doB= doB.toString();
+            my_doB = new Date(my_doB);            
+         var dx= my_doB.getDay(); 
+
+    if (  username === ""){
+        alert("Please enter name....");
+        return false;
+    }else if (doB =="") {
+        alert("Please select date!!!")
+    } else if(male.checked ==true) {
+        
+               
+            alert(`You akan name is ${maleName[dx]} born on ${weekDays[dx]}`);        
+    }else if(female.checked ==true) {        
+              
+            alert(`You akan name is ${femalNames[dx]} born on ${weekDays[dx]}`);
+    }
+}
